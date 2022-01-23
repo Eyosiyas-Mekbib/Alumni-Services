270ff94474eeb9a2abf02d1886723e1dddaec117
yeabserat <yeabsera.seyoum@bitscollege.edu.et>
2022-01-23 16:03:44 +0300

    Services section added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Container from '@mui/material/Container'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Services from './components/Services'
 
 function App() {
   return (
     <Container className='home'>
       <Navbar/>
       <Hero/>
+      <Services/>
     </Container>
   );
 }
